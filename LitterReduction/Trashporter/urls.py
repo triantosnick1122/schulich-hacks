f74c357389174ebd5571711360085789b94c6e41
triantosnick1122 <nicholas.triantos@cs.mcgill.ca>
2023-04-29 11:20:12 -0600

    add get_reports endpoint and method

--- LitterReduction/Trashporter/urls.py
+++ LitterReduction/Trashporter/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("litter_map/", views.litter_map, name="litter_map"),
     path("camera_feed/",views.camera_feed, name="camera_feed"),
     path("camera/",views.camera, name="live_camera"),
-    path("capture_img",views.capture_img, name = "capture_img")
+    path("capture_img",views.capture_img, name = "capture_img"),
+    path("get_reports",views.get_reports, name = "get_reports")
 ]
